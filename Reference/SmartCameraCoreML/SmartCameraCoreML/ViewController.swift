8760d9bea27d7ba050801bfdc885ed4ce9cc07d6
Thiago Oliveira <thiago@outlook.com.br>
2021-09-14 20:42:08 -0300

    Starter project

---
+++ Reference/SmartCameraCoreML/SmartCameraCoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCameraCoreML
+//
+//  Created by Thiago Oliveira on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
